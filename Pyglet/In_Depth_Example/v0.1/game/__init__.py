255f0ef92699810914bf1205c019dad305076027
vivekchadhuvula01 <vivekchadhuvula@gmail.com>
2025-09-15 12:33:16 +0530

    Loading images

--- Pyglet/In_Depth_Example/v0.1/game/__init__.py
+++ Pyglet/In_Depth_Example/v0.1/game/__init__.py
@@ -1,6 +1,12 @@
 import pyglet
 
 game_window = pyglet.window.Window(800,600)
+pyglet.resource.path= ['../resources']
+pyglet.resource.reindex()
+
+player_image =  pyglet.resource.image('player.png')
+bullet_image = pyglet.resource.image('bullet.png')
+asteriod_image = pyglet.resource.image('asteriod.png')
 
 if __name__ == '__main__':
     pyglet.app.run()
